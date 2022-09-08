3164d005cd996f0dd0ee2efaaa7ebd40927330e6
nagudelo90academia <nagudelo90academia@gmail.com>
2022-09-08 11:56:52 -0500

    Modificacion adicional otro comentario

--- src/main/java/co/edu/utp/App.java
+++ src/main/java/co/edu/utp/App.java
@@ -5,6 +5,7 @@ package co.edu.utp;
  *
  */
 // Realizo modificacion para el git
+// Un comentario nuevo para realizar otro cambio y llevar al repo
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
