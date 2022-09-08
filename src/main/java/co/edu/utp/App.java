ab8fd44f7805c395e7f3960140ff17c0fefe2ee7
nagudelo90academia <111032728+nagudelo90academia@users.noreply.github.com>
2022-09-08 11:21:43 -0500

    Comentario para probar git

--- src/main/java/co/edu/utp/App.java
+++ src/main/java/co/edu/utp/App.java
@@ -4,10 +4,9 @@ package co.edu.utp;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+// Realizo modificacion para el git
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
